4eb34f0beb4efb9bfb21691f1143ec7b91c683e2
zhentao <shizhentao@gmail.com>
2016-02-25 11:29:20 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Compare Estimators
+
+This project compares the performance of post-Lasso (BCCH, 2012) and RJIVE (Hansen and Kozbur 2014) with REL and BC-REL
+in a linear IV model.
+
+* The estimation of REL and BC-REL has been done.
+* We need to code up post-Lasso and RJIVE
+
+
+### Functionality
+
+* `master_IV.m` is the master file for the Monte Carlo simulation.
+* `dgpLinearIV.m` generates the data `y`, `x` and `z` in each simulation.
---
+++ master_IV.m
@@ -0,0 +1,34 @@
+global  n y x z m
+
+Rep = 5; % # of Monte Carlo replication
+
+n = 100;
+m = 80;
+
+seed = 301;
+useful = 4; % the number of useful (non-zero) x's
+rho = 0.6; % the endogeneity
+beta0 = [1;1]; % column vector. Good.
+
+
+tic
+r = 1;
+
+while r <= Rep
+
+    seed_v = seed + r_no_repeat;
+    rng(seed_v);
+    
+    [y, x, z] = dgpLinearIV(beta0, rho, useful); % generate the data
+    
+    % with the generated data
+    % use the post-lasso and Hansen Kozbur's RJIVE to estimate the
+    % parameter
+    
+    
+    r
+    toc
+end
+
+
+
