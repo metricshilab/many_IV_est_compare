de8bbf755d9f0e94dc83846f344bad6e65e1ee8f
Eddie-Gao <gaozhan.cuhk@gmail.com>
2016-03-01 21:29:05 +0800

    incorporated with bcch

--- master_IV.m
+++ master_IV.m
@@ -1,9 +1,9 @@
 global  n y x z m
 
-Rep = 5; % # of Monte Carlo replication
+Rep = 500; % # of Monte Carlo replication
 
-n = 100;
-m = 80;
+n = 120; % n takes from [120, 240]
+m = 80; % m takes from [80, 160]
 
 seed = 301;
 useful = 4; % the number of useful (non-zero) x's
@@ -11,10 +11,13 @@ rho = 0.6; % the endogeneity
 beta0 = [1;1]; % column vector. Good.
 
 
-tic
-r = 1;
+beta_lasso_2sls = zeros(2, Rep);
+beta_lasso_fuller = zeros(2, Rep);
+%beta_hat_RJIVE = zeros(2, Rep);
 
-while r <= Rep
+tic
+for r = 1:Rep
+    
     seed_v = seed + r;
     rng(seed_v);
     
@@ -23,11 +26,9 @@ while r <= Rep
     % with the generated data
     % use the post-lasso and Hansen Kozbur's RJIVE to estimate the
     % parameter
+	[beta_lasso_2sls(:, r),~,beta_lasso_fuller(:,r),~,~] = post_lasso(y, x, z);
     
-    
-    r = r + 1;
-    toc
+    %beta_hat_RJIVE(:, r) = RJIVE(y, x, z);
+
 end
-
-
-
+toc
