581ce08b20eb4a569105c43e8dccca0703c57c65
Eddie-Gao <gaozhan.cuhk@gmail.com>
2016-03-07 00:34:48 +0800

    the function implements the RJIVE estimator

---
+++ RJIVE.m
@@ -0,0 +1,27 @@
+function [ beta_rjive ] = RJIVE( y, x, Z )
+
+%This function implements the estimation procedure proposed by Hansen and
+%Kozbur(2014), the regularized JIVE estimator.
+global n m;
+dim_x = size(x,2);
+
+%Need to obtain the const C
+
+Lambda_square = C^2 * m * eye(m);
+F1 = zeros(dim_x, dimx);
+F2 = zeros(dim_x, 1);
+
+for i = 1:n
+    Z_i = Z(i,:)';
+    x_i = x(i,:)';
+    y_i = y(i);
+    PI_i = (Z'*Z - Z_i*Z_i' + Lambda_square) \ (Z'* x - Z_i*x_i');
+    F1 = F1 + PI_i' * Z_i * x_i';
+    F2 = F2 + PI_i' * Z_i * y_i;
+end
+
+beta_hat = F1 \ F2;
+beta_rjive = beta_hat(1);
+
+end
+
